5d1b355b7a4b93a609132c954ce02f65f0a66b3b
Maria Balsamo <mia.balsamo@gmail.com>
2018-01-10 01:11:41 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   end
 
   def execute_transaction
-
+    binding.pry
     if self.sender.valid? == false
       "Transaction rejected. Please check your account balance."
     elsif self.sender.valid? == true
@@ -26,4 +26,7 @@ class Transfer
     end
   end
 
+  def reverse_transfer
+    
+  end
 end
