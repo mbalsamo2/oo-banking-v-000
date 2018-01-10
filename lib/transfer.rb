61437cc798e79f4182151581dfac11b265c1370b
Maria Balsamo <mia.balsamo@gmail.com>
2018-01-10 00:27:50 +0000

    automatically backed up by learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,11 @@
 class Transfer
-  # your code here
+
+  attr_accessor :sender, :receiver, :status
+
+  def initialize(sender, receiver, status = "pending")
+    @sender = sender
+    @receiver = receiver
+    @status = status
+  end
+
 end
